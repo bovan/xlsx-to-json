828ebd34a15cd7cd5feaf86e830d7065f7a4605d
Børge Antonsen <borge.antonsen@dirmin.no>
2017-07-06 12:49:57 +0200

    first commit

---
+++ convert.js
@@ -0,0 +1,12 @@
+  xlsxj = require("xlsx-to-json");
+
+  xlsxj({
+    input: "input.xlsx",
+    output: "output.json"
+  }, function(err, result) {
+    if(err) {
+      console.error(err);
+    }else {
+      console.log(result);
+    }
+  });
